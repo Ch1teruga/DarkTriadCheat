45a574e04602a73c8049b9bdbc2d6b7424d5a807
Ch1truga <gigocheal@gmail.com>
2026-06-05 15:17:24 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,54 +1,148 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ch1teruga/DarkTriadCheat/main/Library.lua"))()
 
-local Window = Library.CreateLib("DarkTrial", "RJTheme3")
+local Window = Library.CreateLib("DarkTriad", "RJTheme3")
 
-local Tab = Window:NewTab("Передвижение")
-local Section = Tab:NewSection("Скорость")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local function GetHumanoid()
+local Character = LocalPlayer.Character
+return Character and Character:FindFirstChildOfClass("Humanoid")
+end
 
 local Speed = 16
-local Enabled = false
+local JumpPower = 50
+local JumpHeight = 7.2
 
--- ввод скорости
-Section:NewTextBox("Скорость", "Укажите скорость", function(speed)
-    local num = tonumber(speed)
-    if num then
-        Speed = num
-    end
+local SpeedEnabled = false
+local JumpPowerEnabled = false
+local JumpHeightEnabled = false
+
+local Tab = Window:NewTab("Передвижение")
+
+local SpeedSection = Tab:NewSection("Скорость")
+
+SpeedSection:NewTextBox("Скорость", "Укажите скорость (деф. 16)", function(value)
+local num = tonumber(value)
+
+
+if num then
+    Speed = num
+end
+
+
 end)
 
--- переключатель
-Section:NewToggle("Обход замедления", "Вкл/Выкл фиксацию скорости", function(state)
-    Enabled = state
+SpeedSection:NewToggle("Применить скорость", "Вкл/Выкл скорость", function(state)
+SpeedEnabled = state
 
-    local char = game.Players.LocalPlayer.Character
-    local humanoid = char and char:FindFirstChildOfClass("Humanoid")
 
-    if humanoid then
-        if Enabled then
-            humanoid.WalkSpeed = Speed
-        else
-            humanoid.WalkSpeed = 16
-        end
+local Humanoid = GetHumanoid()
+
+if Humanoid then
+    if state then
+        Humanoid.WalkSpeed = Speed
+    else
+        Humanoid.WalkSpeed = 16
     end
+end
+
+
 end)
 
--- поддержание скорости
+local JumpPowerSection = Tab:NewSection("JumpPower")
+
+JumpPowerSection:NewTextBox("Сила прыжка", "Укажите силу прыжка (деф. 50)", function(value)
+local num = tonumber(value)
+
+
+if num then
+    JumpPower = num
+end
+
+
+end)
+
+JumpPowerSection:NewToggle("Применить силу прыжка", "Вкл/Выкл JumpPower", function(state)
+JumpPowerEnabled = state
+
+
+if state then
+    JumpHeightEnabled = false
+end
+
+local Humanoid = GetHumanoid()
+
+if Humanoid then
+    Humanoid.UseJumpPower = true
+
+    if state then
+        Humanoid.JumpPower = JumpPower
+    else
+        Humanoid.JumpPower = 50
+    end
+end
+
+
+end)
+
+local JumpHeightSection = Tab:NewSection("JumpHeight")
+
+JumpHeightSection:NewTextBox("Высота прыжка", "Укажите высоту прыжка (деф. 7.2)", function(value)
+local num = tonumber(value)
+
+
+if num then
+    JumpHeight = num
+end
+
+
+end)
+
+JumpHeightSection:NewToggle("Применить высоту прыжка", "Вкл/Выкл JumpHeight", function(state)
+JumpHeightEnabled = state
+
+
+if state then
+    JumpPowerEnabled = false
+end
+
+local Humanoid = GetHumanoid()
+
+if Humanoid then
+    Humanoid.UseJumpPower = false
+
+    if state then
+        Humanoid.JumpHeight = JumpHeight
+    else
+        Humanoid.JumpHeight = 7.2
+    end
+end
+
+
+end)
+
 task.spawn(function()
-    while task.wait(0.1) do
-        if Enabled then
-            local char = game.Players.LocalPlayer.Character
-            local humanoid = char and char:FindFirstChildOfClass("Humanoid")
+while task.wait(0.1) do
+local Humanoid = GetHumanoid()
 
-            if humanoid then
-                humanoid.WalkSpeed = Speed
-            end
+
+    if Humanoid then
+        if SpeedEnabled then
+            Humanoid.WalkSpeed = Speed
+        end
+
+        if JumpPowerEnabled then
+            Humanoid.UseJumpPower = true
+            Humanoid.JumpPower = JumpPower
+        end
+
+        if JumpHeightEnabled then
+            Humanoid.UseJumpPower = false
+            Humanoid.JumpHeight = JumpHeight
         end
     end
-end)
+end
 
 
-local Section = Tab:NewSection("Прыжок")
-
-Section:NewTextBox("Сила прыжка", "Укажите силу прыжка (деф. 50)", function(JumpPower)
-	 workspace.LocalPlayer.Humanoid.JumpPower = JumpPower
 end)
